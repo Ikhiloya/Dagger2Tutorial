c7df3b73fc2a9af9148c01d8218a430959b96daa
Ikhiloya <imokhaiikhiloya@gmail.com>
2020-10-14 08:29:23 +0100

    binding intefaces with @Binds

--- app/src/main/java/com/ikhiloya/imokhai/dagger2tutorial/dagger/DieselEngineModule.java
+++ app/src/main/java/com/ikhiloya/imokhai/dagger2tutorial/dagger/DieselEngineModule.java
@@ -4,13 +4,14 @@ import com.ikhiloya.imokhai.dagger2tutorial.model.DieselEngine;
 import com.ikhiloya.imokhai.dagger2tutorial.model.Engine;
 import com.ikhiloya.imokhai.dagger2tutorial.model.PetrolEngine;
 
+import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 
 @Module
-public class DieselEngineModule {
-    @Provides
-    Engine DieselPetrolEngine(PetrolEngine petrolEngine) {
-        return new DieselEngine();
-    }
+public abstract class DieselEngineModule {
+
+    //To reduce the verbosity of this module, we turn it to an abstract class and bind the engine
+    @Binds
+    abstract Engine bindEngine(DieselEngine dieselEngine);
 }
--- app/src/main/java/com/ikhiloya/imokhai/dagger2tutorial/dagger/PetrolEngineModule.java
+++ app/src/main/java/com/ikhiloya/imokhai/dagger2tutorial/dagger/PetrolEngineModule.java
@@ -3,13 +3,12 @@ package com.ikhiloya.imokhai.dagger2tutorial.dagger;
 import com.ikhiloya.imokhai.dagger2tutorial.model.Engine;
 import com.ikhiloya.imokhai.dagger2tutorial.model.PetrolEngine;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 @Module
-public class PetrolEngineModule {
-    @Provides
-    Engine providePetrolEngine(PetrolEngine petrolEngine) {
-        return new PetrolEngine();
-    }
+public abstract class PetrolEngineModule {
+    @Binds
+    abstract Engine bindEngine(PetrolEngine petrolEngine);
+
 }
